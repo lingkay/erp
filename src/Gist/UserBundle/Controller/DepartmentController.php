d3ac52cadc1fd036b36b5582aaaf270baee72203
Richard Dale <richard.umayan@icloud.com>
2017-05-12 16:43:23 -0700

    added departments

---
+++ src/Gist/UserBundle/Controller/DepartmentController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Gist\UserBundle\Controller;
+
+use Gist\TemplateBundle\Model\CrudController;
+use Gist\UserBundle\Entity\Department;
+use Gist\ValidationException;
+
+class DepartmentController extends CrudController
+{
+    public function __construct()
+    {
+        $this->route_prefix = 'cat_user_department';
+        $this->title = 'Department';
+
+        $this->list_title = 'Departments';
+        $this->list_type = 'dynamic';
+    }
+
+    protected function newBaseClass()
+    {
+        return new Department();
+    }
+
+    protected function getObjectLabel($obj)
+    {
+        return $obj->getDepartmentName();
+    }
+
+    protected function getGridColumns()
+    {
+        $grid = $this->get('gist_grid');
+
+        return array(
+            $grid->newColumn('Department Name', 'getDepartmentName', 'department_name'),
+        );
+    }
+
+    protected function padFormParams(&$params, $user = null)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+
+        return $params;
+    }
+
+    protected function update($o, $data, $is_new = false)
+    {
+//        $uc = $this->get('gist_user');
+
+
+        $o->setDepartmentName($data['department_name']);
+
+
+    }
+
+//    public function toData()
+//    {
+//        $data = array(
+//            'warehouse_id' => $o->getWarehouse()->getID(),
+//        );
+//
+//        return $data;
+//    }
+}
---
+++ src/Gist/UserBundle/Entity/Department.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace Gist\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\ArrayCollection;
+
+use Gist\CoreBundle\Template\Entity\HasGeneratedID;
+use Gist\CoreBundle\Template\Entity\HasCode;
+use Gist\CoreBundle\Template\Entity\HasNotes;
+use Gist\CoreBundle\Template\Entity\TrackCreate;
+
+use DateTime;
+use stdClass;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user_department")
+ */
+
+class Department
+{
+
+
+    use HasGeneratedID;
+    use TrackCreate;
+
+
+
+    /** @ORM\Column(type="string", length=50) */
+    protected $department_name;
+
+
+
+
+
+    public function __construct()
+    {
+        $this->initTrackCreate();
+//        $this->setAcceptSat(false);
+    }
+
+
+
+    public function setDepartmentName($department_name)
+    {
+        $this->department_name = $department_name;
+
+        return $this;
+    }
+
+    public function getDepartmentName()
+    {
+        return $this->department_name;
+    }
+
+
+
+    public function toData()
+    {
+        $data = new \stdClass();
+        $this->dataHasGeneratedID($data);
+        $this->dataTrackCreate($data);
+//        $this->dataHasCode($data);
+
+//        $data->emp_name = $this->employee->getDisplayName();
+//        $data->emp_id = $this->employee->getID();
+//        $data->approved_by = $this->approved_by;
+//        $data->leave_name = $this->emp_leave->getLeaveType()->getName();
+//        $data->leave_id = $this->emp_leave->getLeaveType()->getID();
+//        $data->date_start = $this->date_start;
+//        $data->date_end = $this->date_end;
+//        $data->status = $this->status;
+        return $data;
+    }
+
+}
