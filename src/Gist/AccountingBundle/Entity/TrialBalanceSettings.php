73ca30258d0c804edbbc80ae97a492babf631f86
Jude <chris@quadrantalpha.com>
2018-09-10 20:40:05 +0800

    XZ Report

---
+++ src/Gist/AccountingBundle/Entity/TrialBalanceSettings.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Gist\AccountingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Gist\CoreBundle\Template\Entity\HasType;
+use Gist\CoreBundle\Template\Entity\HasCode;
+use Gist\CoreBundle\Template\Entity\HasStatus;
+use Gist\CoreBundle\Template\Entity\HasGeneratedID;
+use Gist\CoreBundle\Template\Entity\HasNotes;
+use Gist\CoreBundle\Template\Entity\TrackCreate;
+use DateTime;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="acct_balance_settings")
+ */
+
+class TrialBalanceSettings
+{
+    use HasGeneratedID;
+    use TrackCreate;
+
+    const TYPE_ASSET        = "Asset";
+    const TYPE_LIABILITY    = "Liability";
+    const TYPE_CAPITAL      = "Capital";
+    const TYPE_NET_STALES   = "Net Sales";
+    const TYPE_COS          = "Cos";
+    const TYPE_OPEX         = "Opex";
+
+    /**
+     * @ORM\ManyToOne(targetEntity="\Gist\AccountingBundle\Entity\ChartOfAccount")
+     * @ORM\JoinColumn(name="account_id", referencedColumnName="id", nullable=true)
+     */
+    protected $chart_of_account;
+
+    /** @ORM\Column(type="string", length=150) */
+    protected $type;
+
+    public function __construct()
+    {
+        $this->initTrackCreate();
+    }
+
+    public function setAccount($account)
+    {
+        $this->chart_of_account = $account;
+        return $this;
+    }
+
+    public function getAccount()
+    {
+        return $this->chart_of_account;
+    }
+
+    public function setType($type)
+    {
+        $this->type = $type;
+        return $this;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+}
