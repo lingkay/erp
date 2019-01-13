f6a406ba50c7003de0d576dd13f8b3e9a616086f
Richard Dale <rdmumayan@gmail.com>
2019-01-13 21:02:25 +0800

    Updated configs

--- app/autoload.php
+++ app/autoload.php
@@ -6,8 +6,7 @@ use Composer\Autoload\ClassLoader;
 /**
  * @var ClassLoader $loader
  */
-// $loader = require __DIR__.'/../vendor/autoload.php';
-$loader = require '/home/vagrant/gist_erp2/vendor/autoload.php';
+$loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
 
